9a0cfe58300c75578f19bae8a61f6182289731df
Priscilla Normando <priscillanormando@gmail.com>
2021-02-03 23:18:53 +0000

    Models update

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -2,19 +2,19 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
-  ManyToOne,
-  JoinColumn,
   CreateDateColumn,
   UpdateDateColumn
 } from "typeorm"
 
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
-
+  @Column()
   title: string;
-
+  @CreateDateColumn()
   created_at: Date;
-
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
